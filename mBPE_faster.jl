f7d4f504f55e6256c9adddd1f58b16fbb47b4377
Stijn Rommens <stijnrommens@hotmail.nl>
2023-10-05 16:10:33 +0200

    Updates

--- mBPE_faster.jl
+++ mBPE_faster.jl
@@ -2,7 +2,7 @@ cd(@__DIR__)
 using Pkg
 Pkg.activate(".")
 Pkg.instantiate()
-using QuadGK, DifferentialEquations, Plots, BenchmarkTools
+using QuadGK, DifferentialEquations, Plots, BenchmarkTools, StaticArrays, Sundials
 
 kB = 1.3806503e-23;  # Boltzmann constant [J/K]
 elc = 1.6021765e-19; # Elementary charge  [C]
@@ -51,61 +51,30 @@ function U_Cl(z)
     return U
 end;
 
-function U_H(z)
-    if z < 1e10ah[3] # while inside hydrated ion radius
-        U= 1/(4pi*epsilon_o) * elc^2*beta / (1e-10z*4epsilon_w) * exp(-2kappa*1e-10z) - 3.05
+factor_i, Wi, Ui = 0.0, 0.0, 0.0
+factor_j, Wj, Uj = 0.0, 0.0, 0.0
+
+function mPBE!(du, u, p, t)
+    i, j = p
+
+    factor_i = beta * (q[i]*elc)^2 / (2epsilon_w * 4pi * epsilon_o)
+    Wi = quadgk(k -> f(k,ah[i]), 0, 10.0e10)[1] * factor_i
+    if z < 1e10ah[i] # while inside hydrated ion radius
+        Ui = 1000
     else # outside of radius
-        U = 1/(4pi*epsilon_o) * elc^2*beta / (1e-10z*4epsilon_w) * exp(-2kappa*1e-10z)
+        Ui = Wi*1e10ah[i]/z * exp(-2kappa * (1e-10z - ah[i]))
     end
-    return U
-end;
 
-function U_ClO4(z)
-    if z < 1e10ah[4] # while inside hydrated ion radius
-        U = W(4) * 1e10ah[4]/z * exp(-2kappa * (1e-10z - ah[4])) - 2.1
+    factor_j = beta * (q[j]*elc)^2 / (2epsilon_w * 4pi * epsilon_o)
+    Wj = quadgk(k -> f(k,ah[j]), 0, 10.0e10)[1] * factor_j
+    if z < 1e10ah[j] # while inside hydrated ion radius
+        Uj = 1000
     else # outside of radius
-        U = W(4) * 1e10ah[4]/z * exp(-2kappa * (1e-10z - ah[4]))
+        Uj = Wj*1e10ah[j]/z * exp(-2kappa * (1e-10z - ah[j]))
     end
-    return U
-end;
-
-function mPBE(du, u, p, t)
-    i, j, k, l = p
-
-    if i == 1
-        Ui = U_Na(t)
-    end
-    if k == 1
-        Uk = U_Na(t)
-    end
-
-    if i == 3
-        Ui = U_H(t)
-    end
-    if k == 3
-        Uk = U_H(t)
-    end
-
-    if j == 2
-        Uj = U_Cl(t)
-    end
-    if l == 2
-        Ul = U_Cl(t)
-    end
-
-    if j == 4
-        Uj = U_ClO4(t)
-    end
-    if l == 4
-        Ul = U_ClO4(t)
-    end
-
+    
     du[1] = -u[2]
-    if k==0 && l==0
-        du[2] = 1e-20beta * elc^2 / (epsilon_o*epsilon_w) * rho_ion/1*( q[i]*exp(-Ui - q[i] * u[1]) + q[j]*exp(-Uj - q[j] * u[1]))
-    else
-        du[2] = 1e-20beta * elc^2 / (epsilon_o*epsilon_w) * rho_ion/2*( q[i]*exp(-Ui - q[i] * u[1]) + q[j]*exp(-Uj - q[j] * u[1]) + q[k]*exp(-Uk - q[k] * u[1]) + q[l]*exp(-Ul - q[l] * u[1]))
-    end
+    du[2] = 1e-20beta * elc^2 / (epsilon_o*epsilon_w) * rho_ion/2*( q[i]*exp(-Ui - q[i] * u[1]) + q[j]*exp(-Uj - q[j] * u[1]))
 end;
 
 function bc(residual, u, p, t)
@@ -113,35 +82,16 @@ function bc(residual, u, p, t)
     residual[2] = u[1][1] -0.0
 end;
 
-function solver(param)
-    bvp = BVProblem(mPBE, bc, [0.0, 0.0], tspan, param);
-    sol = solve(bvp, Shooting(RadauIIA5(autodiff=false)), save_everystep = false)#, reltol=1e-5, abstol=1e-6)
-    # sol = solve(bvp, Shooting(RadauIIA5(autodiff=false)))
-    return sol
-end;
+param = 1,2;
+bvp = BVProblem(mPBE, bc, [0.0, 0.0], tspan, param);
+sol = solve(bvp, Shooting(RadauIIA5(autodiff=false)))#, save_everystep = false)
+@btime solve(bvp, Shooting(RadauIIA5(autodiff=false)), save_everystep = false)
 
-NaCl   = 1,2,0,0;
-HCl    = 3,2,0,0;
-NaClO4 = 1,4,0,0;
-HClO4  = 3,4,0,0;
-HCl_NaClO4 = 3,2,1,4;
 
-function test(input)
-    solver(input)
-end;
-@btime test(HCl_NaClO4)
 
-NaCl_sol   = solver(NaCl)
-HCl_sol    = solver(HCl)
-NaClO4_sol = solver(NaClO4)
-HClO4_sol  = solver(HClO4)
-HCl_NaClO4_sol = solver(HCl_NaClO4)
 
+###
 NaCl_list   = zeros(Float64, size(NaCl_sol.u)[1], 2);
-HCl_list    = zeros(Float64, size(HCl_sol.u)[1], 2);
-NaClO4_list = zeros(Float64, size(NaClO4_sol.u)[1], 2);
-HClO4_list  = zeros(Float64, size(HClO4_sol.u)[1], 2);
-HCl_NaClO4_list = zeros(Float64, size(HCl_NaClO4_sol.u)[1], 2);
 function pre_plot!(u_list, sol)
     for (i, solu) in enumerate(sol.u)
         u_list[i,1] = sol.t[i]
@@ -150,14 +100,8 @@ function pre_plot!(u_list, sol)
     return u_list
 end;
 NaCl_list   = pre_plot!(NaCl_list, NaCl_sol);
-HCl_list    = pre_plot!(HCl_list, HCl_sol);
-NaClO4_list = pre_plot!(NaClO4_list, NaClO4_sol);
-HClO4_list  = pre_plot!(HClO4_list, HClO4_sol);
-HCl_NaClO4_list = pre_plot!(HCl_NaClO4_list, HCl_NaClO4_sol);
 
-
-plot([NaCl_list[:,1], HCl_list[:,1], NaClO4_list[:,1], HClO4_list[:,1], HCl_NaClO4_list[:,1]], 
-     [NaCl_list[:,2], HCl_list[:,2], NaClO4_list[:,2], HClO4_list[:,2], HCl_NaClO4_list[:,2]],
+plot(NaCl_list[:,1], NaCl_list[:,2],
     label=["NaCl" "HCl" "NaClO4" "HClO4" "HCl + NaClO4"],
     ylims=(-0.45,0.45),
     xlims=(0,60),
