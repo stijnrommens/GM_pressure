8dc32bfa8791b76cf121f25f25c157a035f64d46
Stijn Rommens <stijnrommens@hotmail.nl>
2023-11-02 10:31:50 +0100

    Update data_processor.py

--- Data_processing/data_processor.py
+++ Data_processing/data_processor.py
@@ -1,6 +1,14 @@
+import os
 import numpy as np
+import pandas as pd
 
-x = 2+2
-print(x)
-y=np.array([2, x])
-print(y)
+path = r'u:\Bubble Column\Data\A2 Fiber Probe\231101 - Flow variation in Water\2023-11-01T134048.evt'
+df = pd.read_csv(path, sep='\t')
+
+
+valid = df['Valid']
+df = df.drop()
+# for i, val in enumerate(valid):
+#     print(val)
+    # if val != "1":
+    #     df = df.drop(val)
