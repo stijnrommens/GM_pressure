a3631e22b0fed5f4f882e5c79b0db201486db66b
Stijn Rommens <113170925+stijnrommens@users.noreply.github.com>
2023-11-21 09:19:54 +0100

    Process data for transition concentration

---
+++ data_processing/figures.py
@@ -0,0 +1,47 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+import processing
+
+concentration = processing.mass2conc()
+
+fiber_probe_results = processing.export_fiber_probe()
+velocity_dispersion = np.array((fiber_probe_results[:,2], fiber_probe_results[:,3])) # m/s
+size_dispersion = np.array((fiber_probe_results[:,5], fiber_probe_results[:,6])) # m
+pressure_sensor_results = processing.export_pressure_sensor()
+
+def size_vel_plot(x, y1, y1_error, y2, y2_error):
+    ''' Plot size and velocity from processed fiber probe data. '''
+    
+    fig, ax1 = plt.subplots()
+    ax2 = ax1.twinx()
+    ax1.plot(    x, y1, color="#69b3a2", lw=3)
+    ax1.errorbar(x, y1, yerr=y1_error, fmt='-o', color="#69b3a2", capsize=3)
+    ax2.plot(    x, y2, color="#3399e6", lw=3)
+    ax2.errorbar(x, y2, yerr=y2_error, fmt='-o', color="#3399e6", capsize=3)
+    ax1.set_xlabel("Concentration [M]")
+    ax1.set_ylabel("Velocity [m/s]", color="#69b3a2", fontsize=14)
+    ax1.tick_params(axis="y", labelcolor="#69b3a2")
+    ax1.set_ylim(0, 0.85)
+    ax2.set_ylabel("Diameter [mm]", color="#3399e6", fontsize=14)
+    ax2.tick_params(axis="y", labelcolor="#3399e6")
+    ax2.set_ylim(0,12)
+    fig.suptitle("Bubble properties", fontsize=20)
+    return plt.show()
+size_vel_plot(fiber_probe_results[:,0], fiber_probe_results[:,1], velocity_dispersion, fiber_probe_results[:,4]*1e3, size_dispersion*1e3)
+
+
+def holdup_plot(x, y1, y2):
+    ''' Plot gas holdup from processed fiber probe and pressure sensor data. '''
+    
+    fig, ax1 = plt.subplots()
+    ax1.plot(x, y1, color="#3399e6", lw=3, label='Pressure sensor')
+    ax1.plot(x, y2, color="#69b3a2", lw=3, label='Fiber probe')
+    ax1.set_xlabel("Concentration [M]")
+    ax1.set_ylabel("Gas holdup [-]", fontsize=14)
+    ax1.tick_params(axis="y")
+    # ax1.set_ylim(0, 0.1)
+    fig.suptitle("Gas holdup", fontsize=20)
+    plt.legend()
+    return plt.show()
+holdup_plot(fiber_probe_results[:,0], pressure_sensor_results[:,-1], fiber_probe_results[:,-1])
--- data_processing/processing.py
+++ data_processing/processing.py
@@ -3,70 +3,95 @@ import openpyxl
 import numpy as np
 import pandas as pd
 from nptdms import TdmsFile
-import matplotlib.pyplot as plt
 
 import calibration
 
-excel_file = r'\\tudelft.net\student-homes\R\srommens\My Documents\GitHub\GM_pressure\data_processing\input_file.xlsx'
-fiber_probe_path = pd.read_excel(excel_file, sheet_name='Measurements', header=None, index_col=0, nrows=1)
-fiber_probe_path = fiber_probe_path.to_numpy()[0][0]
-pressure_sensor_path = pd.read_excel(excel_file, sheet_name='Measurements', header=0, index_col=0, nrows=1)
-pressure_sensor_path = pressure_sensor_path.to_numpy()[0][0]
-files = pd.read_excel(excel_file, sheet_name='Measurements', header=3)
-files = files.to_numpy()
+# Transition_NaCl
+# DryRun_2
 
-calibration_path = pd.read_excel(excel_file, sheet_name='Calibration', header=None, index_col=0, nrows=1)
-calibration_path = calibration_path.to_numpy()[0][0]
+# --- Experiment ---
+excel_file = r'\\tudelft.net\student-homes\R\srommens\My Documents\GitHub\GM_pressure\data_processing\input_file.xlsx'
+fiber_probe_path = pd.read_excel(excel_file, sheet_name='Transition_NaCl', header=None, index_col=0, nrows=1)
+fiber_probe_path = fiber_probe_path.to_numpy()[0][0]
+pressure_sensor_path = pd.read_excel(excel_file, sheet_name='Transition_NaCl', header=0, index_col=0, nrows=1)
+pressure_sensor_path = pressure_sensor_path.to_numpy()[0][0]
+files = pd.read_excel(excel_file, sheet_name='Transition_NaCl', header=3)
+files = files.to_numpy()
+param = files[:,0] # height, weight, etc.
+liquid_height   = files[:,3] # m, water height from sensor with no flow
+depleted_height = files[:,4] # m, correction for water loss in bottom port (0.104 mm/min)
+density_height  = files[:,5] # m, correction for density increase by salt
+
+# --- Calibration ---
+calibration_path  = pd.read_excel(excel_file, sheet_name='Calibration', header=None, index_col=0, nrows=1)
+calibration_path  = calibration_path.to_numpy()[0][0]
 calibration_files = pd.read_excel(excel_file, sheet_name='Calibration', header=2)
 calibration_files = calibration_files.to_numpy()
 
+# --- Constants ---
 rho = 997 # kg/m3, density of water
-g = 9.81  # m/s2, gravitational constant
+g = 9.81 # m/s2, gravitational constant
+radius = 150e-3/2 # m, inside radius column
+sensor_height = 748e-3 # m, height pressure sensor from sparger
+mw_additive = 58.44 # g/mol, molar weight of additive (if present)
 
-# radius = 192e-3/2    # m, inside radius column
-radius = 150e-3/2    # m, inside radius column
 Ac = np.pi*radius**2 # m2, cross-sectional area column
-
-# sensor_height = 563e-3+16e-3 # m, height pressure sensor from sparger
-sensor_height = 752e-3 # m, height pressure sensor from sparger
-# liquid_height = 224e-3       # m, water height from sensor with no flow
-liquid_height = 115e-3       # m, water height from sensor with no flow
+volume_L = Ac*(sensor_height + liquid_height - depleted_height) # m3
 
 
 def fiber_probe(files, folder):
     ''' From the measurements, extract fiber probe information. '''
     
     results = []
-    for file in files:
-        param = file[0]     # height, flow, etc.
+    for i, file in enumerate(files):
+        param = file[0]     # height, weight, etc.
         file_name = file[1] # -
         
-        # Load 1st file
+        # Load the measurement files in dataframes + get void fraction
         path = folder + file_name + '.evt'
         df = pd.read_csv(path, sep='\t', decimal=',')
-
+        path_stream = folder + file_name + '_stream.evt'
+        df_stream = pd.read_csv(path_stream, sep='\t', decimal=',')
+        arrival  = df_stream['Arrival']
+        duration = df_stream['Duration']
+        void_fraction = sum(duration)/arrival.iloc[-1] # -
+        
+        # Check if variable has multiple measurement files + add to dataframes
+        prev_param_count = np.count_nonzero(files[:i+1,0] == param) # Check if same parameter has not been passed previously
+        if prev_param_count > 1:
+            continue
+        tot_param_count = np.count_nonzero(files[:,0] == param) # Get all files with same parameter
+        if tot_param_count > 1:
+            for k, extra_file in enumerate(files[i+1:tot_param_count]):
+                extra_param = extra_file[0]
+                extra_file_name = extra_file[1]
+                extra_path = folder + extra_file_name + '.evt'
+                extra_df = pd.read_csv(extra_path, sep='\t', decimal=',')
+                df = pd.concat([df, extra_df])     
+                extra_stream_path = folder + extra_file_name + '_stream.evt'
+                extra_stream_df = pd.read_csv(extra_stream_path, sep='\t', decimal=',')
+                arrival  = extra_stream_df['Arrival']
+                duration = extra_stream_df['Duration']
+                void_fraction += sum(duration)/arrival.iloc[-1] # -
+                
         df_valid = df[df.Valid == 1] # Only valid bubbles
 
         # Obtain velocity and size
-        velocity = df_valid['Veloc']
-        mean_velocity = np.mean(velocity) # m/s
-        std_velocity = np.std(velocity)   # m/s
+        velocity = df_valid['Veloc'].sort_values()
+        lower_velocity, median_velocity, upper_velocity = np.percentile(velocity, [25, 50, 75]) # m/s
+        lower_velocity = abs(lower_velocity - median_velocity) # m/s
+        upper_velocity = abs(upper_velocity - median_velocity) # m/s
 
-        size = df_valid['Size']*1e-6
-        d32 = sum(size**3)/sum(size**2)   # m
-        # d32 = np.mean(size)
-        std_size = np.std(np.array(size)) # m
+        size = 1e-6*df_valid['Size'].sort_values()
+        # d32 = sum(size**3)/sum(size**2)
+        lower_size, median_size, upper_size = np.percentile(size, [25, 50, 75]) # m
+        lower_size = abs(lower_size - median_size) # m
+        upper_size = abs(upper_size - median_size) # m
 
-        # Load 2nd file
-        path_stream = folder + file_name + '_stream.evt'
-        df_stream = pd.read_csv(path_stream, sep='\t', decimal=',')
-
-        # Obtain gas holdup
-        arrival = df_stream['Arrival']
-        duration = df_stream['Duration']
-        void_fraction = sum(duration)/arrival[len(arrival)-1]
-
-        results.append([param, mean_velocity, std_velocity, d32, std_size, void_fraction])
+        # Obtain average gas holdup
+        void_fraction /= tot_param_count # -
+        
+        results.append([param, median_velocity, lower_velocity, upper_velocity, median_size, lower_size, upper_size, void_fraction])
     return np.array(results)
 fiber_probe_results = fiber_probe(files, fiber_probe_path)
 
@@ -75,7 +100,7 @@ def pressure_sensor(files, fit, folder):
     ''' From the measured mean voltage and the fitted line, obtain the gas holdup. '''
     
     results = []
-    for file in files:
+    for i, file in enumerate(files):
         param = file[0]     # height, flow, etc.
         file_name = file[2] # -
         
@@ -90,8 +115,8 @@ def pressure_sensor(files, fit, folder):
         mean_voltage = np.mean(voltage) # V
         
         gas_height = fit[0]*mean_voltage + fit[1]
-        volume_L = Ac * sensor_height # m3
-        volume_G = Ac * (gas_height-liquid_height) # m3
+        volume_L = Ac * sensor_height + depleted_height[i] + density_height[i] # m3
+        volume_G = Ac * (gas_height - liquid_height[i]) # m3
         holdup = volume_G/volume_L # -
         
         results.append([param, mean_voltage, gas_height, holdup])
@@ -100,29 +125,12 @@ fit = calibration.calibration_fit(calibration_files, calibration_path)
 pressure_sensor_results = pressure_sensor(files, fit, pressure_sensor_path)
 
 
-# Plot velocity and size
-fig, ax1 = plt.subplots()
-ax2 = ax1.twinx()
-ax1.plot(   fiber_probe_results[:,0]/(Ac*60*1000), fiber_probe_results[:,1],     color="#69b3a2", lw=3)
-ax1.errorbar(fiber_probe_results[:,0]/(Ac*60*1000), fiber_probe_results[:,1], yerr=fiber_probe_results[:,2], fmt='-o', color="#69b3a2")
-ax2.plot(   fiber_probe_results[:,0]/(Ac*60*1000), fiber_probe_results[:,3]*1e3, color="#3399e6", lw=3)
-ax2.errorbar(fiber_probe_results[:,0]/(Ac*60*1000), fiber_probe_results[:,3]*1e3, yerr=fiber_probe_results[:,4]*1e3, fmt='-o', color="#3399e6")
-ax1.set_xlabel("Superficial gas velocity [m/s]")
-ax1.set_ylabel("Bubble mean velocity [m/s]", color="#69b3a2", fontsize=14)
-ax1.tick_params(axis="y", labelcolor="#69b3a2")
-ax2.set_ylabel("d32 [mm]", color="#3399e6", fontsize=14)
-ax2.tick_params(axis="y", labelcolor="#3399e6")
-fig.suptitle("Bubble properties", fontsize=20)
-plt.show()
+def mass2conc():
+    ''' Run this function to convert mass (an expimental variable) -> concentration. '''
+    return param / mw_additive / (1000*volume_L) # M, convert added weight to concentration
 
+def export_fiber_probe():
+    return fiber_probe_results
 
-# Plot gas holdup
-fig, ax1 = plt.subplots()
-ax1.plot(pressure_sensor_results[:,0]/(Ac*60*1000), pressure_sensor_results[:,-1], color="#3399e6", lw=3, label='Pressure sensor')
-ax1.plot(fiber_probe_results[:,0]/(Ac*60*1000),     fiber_probe_results[:,-1],     color="#69b3a2", lw=3, label='Fiber probe')
-ax1.set_xlabel("Superficial gas velocity [m/s]")
-ax1.set_ylabel("Gas holdup [-]", fontsize=14)
-ax1.tick_params(axis="y")
-fig.suptitle("Gas holdup", fontsize=20)
-plt.legend()
-plt.show()
+def export_pressure_sensor():
+    return pressure_sensor_results
