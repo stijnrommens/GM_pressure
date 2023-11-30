911ce7f4053eb596a761ca8cbbd492c7539e5c23
Stijn Rommens <113170925+stijnrommens@users.noreply.github.com>
2023-11-30 16:58:48 +0100

    Generalizing adsorption functions

---
+++ mBPE/inputs.jl
@@ -0,0 +1,106 @@
+cd(@__DIR__)
+using Pkg
+Pkg.activate(".")
+using QuadGK , Plots
+
+
+### ---------- Concentration input ----------
+ionS = 0.1; # Ionic strength [M]
+
+
+
+### ---------- Constants ----------
+kB = 1.3806503e-23;  # Boltzmann constant [J/K]
+elc = 1.6021765e-19; # Elementary charge  [C]
+Avog = 6.0221415e23; # Avogadro constant  [1/mol]
+epsilon_o = 8.85418782e-12; # Vacuum's permittivity [F/m]
+epsilon_w = 78.3;           # Water's permittivity [F/m]
+T = 297.15; # Temperature [K]
+
+rho_ion = ionS * Avog * 1000;
+beta = 1 / (kB * T);
+STconst = -1e-6*1e10*beta/Avog;
+kappa = sqrt(2000*elc^2*Avog*ionS*beta/(epsilon_w*epsilon_o));
+boundary = 10e10/kappa;
+
+
+f(k, a) = (k*(sqrt(kappa^2 + k^2)*cosh(k*a) - k*sinh(k*a))) / (sqrt(kappa^2 + k^2)*(sqrt(kappa^2 + k^2)*cosh(k*a) + k*sinh(k*a)));
+function W(q, ah)
+    factor = beta * (q*elc)^2 / (2epsilon_w * 4pi * epsilon_o)
+    W = quadgk(k -> f(k,ah), 0, 10.0e10)[1] * factor
+end;
+
+
+
+### ---------- Ion specific functions ----------
+function U_alpha(t, ah, kappa, W)
+    if t < 1e10ah # while inside hydrated ion radius
+        U = 1000
+    else # outside of radius
+        U = W*1e10ah/t * exp(-2kappa * (1e-10t - ah))
+    end
+end;
+W_Na   = W(+1, 2.50e-10);
+U_Na(t) = U_alpha(t, 2.50e-10, kappa, W_Na);
+Na   = (+1, U_Na);
+W_Cl   = W(-1, 2.00e-10);
+U_Cl(t) = U_alpha(t, 2.00e-10, kappa, W_Cl);
+Cl   = (-1, U_Cl);
+
+function U_beta1(z, ah, kappa, elc, beta, epsilon_o, epsilon_w)
+    if z < 1e10ah # while inside hydrated ion radius
+        U = 1/(4pi*epsilon_o) * elc^2*beta / (1e-10z*4epsilon_w) * exp(-2kappa*1e-10z) - 3.05
+    else # outside of radius
+        U = 1/(4pi*epsilon_o) * elc^2*beta / (1e-10z*4epsilon_w) * exp(-2kappa*1e-10z)
+    end
+end;
+U_H(t) = U_beta1(t, 1.97e-10, kappa, elc, beta, epsilon_o, epsilon_w)
+W_H    = W(+1, 1.97e-10);
+H    = (+1, U_H);
+
+function U_beta2(z, ah, kappa, W)
+    if z < 1e10ah # while inside hydrated ion radius
+        U = W * 1e10ah/z * exp(-2kappa * (1e-10z - ah)) - 2.1
+    else # outside of radius
+        U = W * 1e10ah/z * exp(-2kappa * (1e-10z - ah))
+    end
+end;
+W_ClO4 = W(-1, 2.83e-10);
+U_ClO4(t) = U_beta2(t, 2.83e-10, kappa, W_ClO4)
+ClO4 = (-1, U_ClO4);
+
+# NH4  = (+1, 1.50e-10, W(+1, 1.50e-10));
+# SO4  = (-2, 3.79e-10, W(-2, 3.79e-10));
+
+ion_tot = (Na, Cl, H, ClO4); # The ions present in your system
+n_salts = length(ion_tot)/2
+
+
+
+### ---------- Calculate ----------
+include("solver.jl")
+
+print("Surface excess           = ", sol2[1], " Å")
+print("\nSurface tension          = ", sol2[2], " mN/m.M")
+print("\nElectrostatic potential  = ", 1000*last(sol.u)[1], " mV")
+print("\nGibbs-Marangoni pressure = ", sol2[3], " Pa")
+
+
+
+### ---------- Plot ----------
+# plot(sol.t, reduce(hcat, sol.u)[1,:],
+#     ylims=(-0.45,0.45),
+#     xlims=(0,60),
+#     ylabel="Potential [V]",
+#     xlabel="z [Å]"
+# )
+
+# plot(time_range, sol2[1],
+#     labels=["H" "Cl"],
+#     ylims=(0,0.8),
+#     xlims=(0,12),
+#     ylabel="Concentration [M]",
+#     xlabel="z [Å]")
+# scatter!(time_range, sol2[1],
+#     ylims=(-2,8),
+#     xlims=(0,12))
---
+++ mBPE/solver.jl
@@ -0,0 +1,65 @@
+cd(@__DIR__)
+using Pkg
+Pkg.activate(".")
+using DifferentialEquations, Trapz #,BenchmarkTools
+
+
+### ---------- modified Poisson-Boltzmann equation ----------
+function mPBE!(du, u, p, t)
+    constants, ion_tot, n_salts = p
+    beta, elc, epsilon_o, epsilon_w, rho_ion = constants
+
+    Gads_epot = 0
+    for ion in ion_tot
+        Gads_epot += ion[1]*exp(-(ion[2])(t) - ion[1] * u[1])        
+    end
+
+    du[1] = -u[2]
+    du[2] = 1e-20beta * elc^2 / (epsilon_o*epsilon_w) * (rho_ion*Gads_epot) / n_salts
+    nothing
+end;
+
+function bc!(residual, u, p, t)
+    residual[1] = u[end][2] -0.0
+    residual[2] = u[1][1] -0.0
+    nothing
+end;
+tspan = (boundary, 0.01);
+u0  = [0.0, 0.0];
+mBPE_constants = (beta, elc, epsilon_o, epsilon_w, rho_ion);
+mPBE_param = (mBPE_constants, ion_tot, n_salts);
+bvp = BVProblem(mPBE!, bc!, u0, tspan, mPBE_param);
+sol = solve(bvp, Shooting(RadauIIA5(autodiff=false)));
+# @btime sol = solve(bvp, Shooting(RadauIIA5(autodiff=false)))
+
+
+
+### ---------- Gibbs-Marangoni pressure ----------
+function pGM!(time, potential, p)
+    constants, ion_tot, n_salts = p
+    STconst, Avog, ionS, beta = constants
+
+    conc_matrix = zeros(Float64, size(time)[1], length(ion_tot))
+    for (index, t) in enumerate(time)
+        for (index2, ion) in enumerate(ion_tot)
+            conc_matrix[index,index2] = exp(-(ion[2])(t)-ion[1]*potential(t)[1]) - 1
+
+        end
+    end
+
+    gammacon = zeros(1, length(ion_tot))
+    for (index, ion) in enumerate(ion_tot)
+        gammacon[index] = trapz(time, conc_matrix[:, index])
+    end
+
+    tension = sum(gammacon)/(STconst*n_salts)
+    gammacon_squared = sum(map(x->x^2, gammacon))
+    pGM = 4Avog * ionS/n_salts*1000 * gammacon_squared*1e-20 / (beta*(10e-9)^2)
+
+    return (gammacon, tension, pGM);
+end;
+time_range = range(0.01, boundary, 1000001);
+pGM_constants = (STconst, Avog, ionS, beta);
+pGM_param = (pGM_constants, ion_tot, n_salts);
+sol2 = pGM!(time_range, sol, pGM_param);
+# @btime sol2 = pGM!(time_range, sol, param, ionS)
