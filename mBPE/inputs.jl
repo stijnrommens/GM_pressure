c03faed28c12c0d85b046faa28565371afaa35e0
stijnrommens <stijnrommens@hotmail.nl>
2024-01-17 16:41:46 +0100

    Using ionic concentrations

--- mBPE/inputs.jl
+++ mBPE/inputs.jl
@@ -1,12 +1,7 @@
 cd(@__DIR__)
 using Pkg
 Pkg.activate(".")
-using Printf, QuadGK #,Plots
-
-
-### ---------- Concentration input ----------
-ionS = 0.1; # Ionic strength [M]
-
+using Printf, QuadGK ,Plots
 
 
 ### ---------- Constants ----------
@@ -17,12 +12,41 @@ epsilon_o = 8.85418782e-12; # Vacuum's permittivity [F/m]
 epsilon_w = 78.3;           # Water's permittivity [F/m]
 T = 297.15;                 # Temperature [K]
 h = 10e-9;                  # Separation distance [nm] where pGM is calculated at in Duignan (2021)
-
-rho_ion = ionS * Avog * 1000;   # Ion density [particles/L] -> [particles/m3]
 beta = 1 / (kB * T);            # Thermodynamic beta [1/J] = [s2/kg.m2] = [1/N.m]
 STconst = -1e-6*1e10*beta/Avog; # Surfce tension constant [mol/N.m] -> [M.m.Å/mN]
-kappa = sqrt(2 * elc^2 * Avog * ionS*1000 * beta / (epsilon_w*epsilon_o)); # Inverse Debye lenght [1/m]
-boundary = 10e10/kappa;         # 10x Debye lenght [Å]
+
+
+
+### ---------- Input ----------
+# function give_ions(ion_list)
+#     # ionS = 0.5 *
+#     ion_tot = zeros(1, length(ion_list))
+#     for (index, ion) in enumerate(ion_list)
+#         W = W(ion[2], ion[2])
+#         U(t) = U_alpha(t, ion[3], kappa, W)
+#         ion_tot[index] = [ion[1], ion[2], U]
+
+#     return ion_tot
+# end;
+# ion_tot = give_ions(ion_list)
+
+
+# ionC = 0.1; # Ionic concentration [M]
+# ionS = 0.1; # Ionic strength [M] CALCULATE FROM ionC FOR EACH SALT!
+n_salts = 2; # Number of salts [-]
+C = 0.01;
+print_flag = true
+
+c_Na,   q_Na,   r_Na   = C*2, +1, 2.50e-10; # Ionic concentration [M], Charge [-], Hydrated radius [m] from Levin (2009)
+c_Cl,   q_Cl,   r_Cl   = 0, -1, 2.00e-10; # Levin (2010)
+c_NH4,  q_NH4,  r_NH4  = 0, +1, 2.50e-10; # Kielland (1937)
+c_SO4,  q_SO4,  r_SO4  = C, -2, 3.79e-10; # Levin (2010)
+c_H,    q_H,    r_H    = 0.02/2, +1, 1.97e-10; # Levin (2009)
+c_ClO4, q_ClO4, r_ClO4 = 0.02/2, -1, 2.83e-10; # Levin (2009)
+
+ionS = 0.5 * (c_Na*q_Na^2 + c_SO4*q_SO4^2) # Ionic strength [M]
+kappa = sqrt(2 * elc^2 * Avog * ionS*1000 * beta / (epsilon_w*epsilon_o)); # Inverse Debye-Hückel lenght [1/m]
+boundary = 10e10/kappa;         # 10x Debye-Hückel lenght [Å]
 
 
 f(k, a) = (k*(sqrt(kappa^2 + k^2)*cosh(k*a) - k*sinh(k*a))) / (sqrt(kappa^2 + k^2)*(sqrt(kappa^2 + k^2)*cosh(k*a) + k*sinh(k*a))); # [1/m2 / 1/m2] -> [-]?
@@ -49,42 +73,63 @@ function U_alpha(t, ah, kappa, W)
         U = W*1e10ah/t * exp(-2kappa * (1e-10t - ah)) # Gibbs adsorption energy [J]
     end
 end;
-q_Na, r_Na = +1, 2.50e-10; # Charge [-], Hydrated radius [m] from Levin (2009)
 W_Na   = W(q_Na, r_Na);
 U_Na(t) = U_alpha(t, r_Na, kappa, W_Na);
-Na   = (q_Na, U_Na);
+Na   = (c_Na, q_Na, U_Na);
 
-q_Cl, r_Cl = -1, 2.00e-10; # Levin (2010)
 W_Cl   = W(q_Cl, r_Cl);
 U_Cl(t) = U_alpha(t, r_Cl, kappa, W_Cl);
-Cl   = (q_Cl, U_Cl);
+Cl   = (c_Cl, q_Cl, U_Cl);
 
-q_NH4, r_NH4 = +1, 2.50e-10; # Kielland (1937)
 W_NH4 = W(q_NH4, r_NH4);
 U_NH4(t) = U_alpha(t, r_NH4, kappa, W_NH4);
-NH4 = (q_NH4, U_NH4);
+NH4 = (c_NH4, q_NH4, U_NH4);
 
-q_SO4, r_SO4 = -2, 3.79e-10; # Levin (2010)
 W_SO4 = W(q_SO4, r_SO4);
 U_SO4(t) = U_alpha(t, r_SO4, kappa, W_SO4);
-SO4 = (q_SO4, U_SO4);
+SO4 = (c_SO4, q_SO4, U_SO4);
 
+
+function U_beta1(z, ah, kappa, elc, beta, epsilon_o, epsilon_w)
+    if z < 1e10ah # while inside hydrated ion radius
+        U = 1/(4pi*epsilon_o) * elc^2*beta / (1e-10z*4epsilon_w) * exp(-2kappa*1e-10z) - 3.05
+    else # outside of radius
+        U = 1/(4pi*epsilon_o) * elc^2*beta / (1e-10z*4epsilon_w) * exp(-2kappa*1e-10z)
+    end
+end;
+U_H(t) = U_beta1(t, r_H, kappa, elc, beta, epsilon_o, epsilon_w)
+W_H    = W(q_H, r_H);
+H      = (c_H, q_H, U_H);
+
+
+function U_beta2(z, ah, kappa, W)
+    if z < 1e10ah # while inside hydrated ion radius
+        U = W * 1e10ah/z * exp(-2kappa * (1e-10z - ah)) - 2.1
+    else # outside of radius
+        U = W * 1e10ah/z * exp(-2kappa * (1e-10z - ah))
+    end
+end;
+W_ClO4 = W(q_ClO4, r_ClO4);
+U_ClO4(t) = U_beta2(t, r_ClO4, kappa, W_ClO4)
+ClO4 = (c_ClO4, q_ClO4, U_ClO4);
 
 
 ### ---------- Ions/salts input ----------
-ion_tot = (Na, Na, SO4, NH4, Cl); # The ions composing the salts
-n_salts = 2;                      # Number of salts [-]
+ion_tot = (Na, SO4)#, NH4, Cl); # The ions composing the salts
 
 
 
 ### ---------- Calculate ----------
 include("solver.jl")
 
-print("\n\nResults:")
-print("\n   • Surface excess × charge  = ", round.(sol2[1]; digits=3), " = ", round.(sum(sol2[1]); digits=3), " Å")
-@printf("\n   • Surface tension          = %.3f mN/m.M", sol2[2])
-@printf("\n   • Electrostatic potential  = %.3e mV", 1000*last(sol.u)[1])
-@printf("\n   • Gibbs-Marangoni pressure = %.3f Pa", sol2[3])
+if print_flag == true
+    print("\n\nResults:")
+    print("\n   • Surface excess × charge  = ", round.(sol2[2]; digits=3), " = ", round.(sum(sol2[2]); digits=3), " Å")
+    @printf("\n   • Surface tension          = %.3f mN/m.M", sol2[3])
+    @printf("\n   • Electrostatic potential  = %.3e mV", 1000*last(sol.u)[1])
+    @printf("\n   • Gibbs-Marangoni pressure = %.3f Pa", sol2[4])
+    @printf("\n \U1FAE7")
+end
 
 
 
@@ -97,11 +142,11 @@ print("\n   • Surface excess × charge  = ", round.(sol2[1]; digits=3), " = ",
 # )
 
 # plot(time_range, sol2[1],
-#     labels=["H" "Cl"],
-#     ylims=(0,0.8),
-#     xlims=(0,12),
+#     labels=["Na" "Na" "SO4"],
+#     # ylims=(-0.005,0.005),
+#     xlims=(0,boundary),
 #     ylabel="Concentration [M]",
 #     xlabel="z [Å]")
 # scatter!(time_range, sol2[1],
-#     ylims=(-2,8),
-#     xlims=(0,12))
+    # ylims=(-2,8),
+    # xlims=(0,boundary))
--- mBPE/solver.jl
+++ mBPE/solver.jl
@@ -2,33 +2,37 @@ using BoundaryValueDiffEq, OrdinaryDiffEq, Trapz #,BenchmarkTools
 
 
 ### ---------- Check input ----------
-print("Solver messages:")
-function check_input(ion_tot, n_salts)
+if print_flag == true
+    print("Solver messages:")
+end
+
+function check_input(ion_tot, n_salts, print_flag)
     """
     Check if sum of charges = 0  and if number of salts is correct.
     """
+    if print_flag == true
+        charge_tot = 0
 
-    charge_tot = 0
+        for (index, ion) in enumerate(ion_tot)
+            charge_tot += ion[1]*ion[2] # Sum of charges [-]
+        end
 
-    for (index, ion) in enumerate(ion_tot)
-        charge_tot += ion[1] # Sum of charges [-]
-    end
+        if charge_tot != 0
+            print("\n   ✖ Warning: Sum of charges is NOT 0!")
+            # error("Inputs NOT correct!")
+        else
+            print("\n   ✔ Sum of charges is 0.")
+        end
 
-    if charge_tot != 0
-        print("\n   ✖ Warning: Sum of charges is NOT 0!")
-        error("Inputs NOT correct!")
-    else
-        print("\n   ✔ Sum of charges is 0.")
-    end
-
-    if length(ion_tot)/n_salts > 3
-        print("\n   ✖ Warning: Number of salts may NOT correct!")
-    else
-        print("\n   ✔ Number of salts is correct.")
+        if length(ion_tot)/n_salts > 3
+            print("\n   ✖ Warning: Number of salts may NOT correct!")
+        else
+            print("\n   ✔ Number of salts is correct.")
+        end
     end
     nothing
 end;
-check_input(ion_tot, n_salts)
+check_input(ion_tot, n_salts, print_flag)
 
 
 ### ---------- modified Poisson-Boltzmann equation ----------
@@ -39,15 +43,16 @@ function mPBE!(du, u, p, t)
     """
 
     constants, ion_tot, n_salts = p
-    beta, elc, epsilon_o, epsilon_w, rho_ion = constants
+    beta, elc, epsilon_o, epsilon_w, Avog = constants
 
     Gads_epot = 0
     for ion in ion_tot
-        Gads_epot += ion[1]*exp(-(ion[2])(t) - ion[1] * u[1]) # Sum of exponentials [-]
+        rho_ion = ion[1] * Avog * 1000 # Ion density [particles/L] -> [particles/m3]
+        Gads_epot += rho_ion * ion[2] * exp(-(ion[3])(t) - ion[2] * u[1]) # Sum of exponentials [-]
     end
 
     du[1] = -u[2] # Second derivative [?]
-    du[2] = 1e-20beta * elc^2 / (epsilon_o*epsilon_w) * (rho_ion*Gads_epot) / n_salts # First derivative [?]
+    du[2] = 1e-20beta * elc^2 / (epsilon_o*epsilon_w) * Gads_epot #/ n_salts # First derivative [?] IS n_salts actually needed, or did Tim used it to use comparable numbers for mixtures?
     nothing
 end;
 
@@ -62,7 +67,7 @@ function bc!(residual, u, p, t)
 end;
 tspan = (boundary, 0.01); # Distance [Å]
 u0  = [0.0, 0.0];
-mBPE_constants = (beta, elc, epsilon_o, epsilon_w, rho_ion);
+mBPE_constants = (beta, elc, epsilon_o, epsilon_w, Avog);
 mPBE_param = (mBPE_constants, ion_tot, n_salts);
 bvp = BVProblem(mPBE!, bc!, u0, tspan, mPBE_param);
 sol = solve(bvp, Shooting(RadauIIA5(autodiff=false)), abstol=1e-12, reltol= 1e-6);
@@ -71,7 +76,7 @@ sol = solve(bvp, Shooting(RadauIIA5(autodiff=false)), abstol=1e-12, reltol= 1e-6
 
 
 ### ---------- Gibbs-Marangoni pressure ----------
-function pGM!(time, potential, p)
+function pGM!(time, potential, p, print_flag)
     """
     Use electrostatic potential to obtain the Gibbs-Marangoni pressure (at 10 nm separation).
     """
@@ -82,31 +87,39 @@ function pGM!(time, potential, p)
     conc_matrix = zeros(Float64, size(time)[1], length(ion_tot))
     for (index, t) in enumerate(time)
         for (index2, ion) in enumerate(ion_tot)
-            conc_matrix[index, index2] = exp(-(ion[2])(t)-ion[1]*potential(t)[1]) - 1 # Concentration profile [M/M] = [-]
+            conc_matrix[index, index2] = exp(-(ion[3])(t)-ion[2]*potential(t)[1]) - 1 # Fractionate ionic concentration profile [M/M] = [-], Eq.6
         end
     end
 
     gammacon = zeros(1, length(ion_tot))
     gammacon_q = zeros(1, length(ion_tot))
     for (index, ion) in enumerate(ion_tot)
-        gammacon[index] = trapz(time, conc_matrix[:, index]) # Surface excess over cocentration [M.Å/M] = [Å]
-        gammacon_q[index] = gammacon[index]*ion[1] # Surface excess × charge [Å]
+        gammacon[index] = trapz(time, conc_matrix[:, index]) # Surface excess over cocentration [M.Å/M] = [Å], Eq.6
+        gammacon_q[index] = gammacon[index]*ion[2] # Surface excess × charge [Å]
     end
 
-    if sum(gammacon_q) > 0.05
-        print("\n   ✖ Warning: Electroneutrality condition is NOT satisfied!")
-    else
-        print("\n   ✔ Electroneutrality condition is satisfied.")
+    if print_flag == true
+        if sum(gammacon_q) > 0.05
+            print("\n   ✖ Warning: Electroneutrality condition is NOT satisfied!")
+        else
+            print("\n   ✔ Electroneutrality condition is satisfied.")
+        end
     end
 
     tension = sum(gammacon)/(STconst*n_salts) # Surface tension [Å] / [M.m.Å/mN] -> [mN/m.M]
-    gammacon_squared = sum(map(x->x^2, gammacon)) # Surface excess squared [Å^2]
-    pGM = 4Avog * ionS/n_salts*1000 * gammacon_squared*1e-20 / (beta*h^2) # Gibbs-Marangoni pressure [m4.mol.kg/m5.mol.s2] -> [Pa]
+    # tension = sum(gammacon)/STconst # Surface tension [Å] / [M.m.Å/mN] -> [mN/m.M]
+    
+    pGM = 0
+    for (index, ion) in enumerate(ion_tot)
+        # gammacon_squared = sum(map(x->x^2, gammacon)) # Surface excess squared [Å^2]
+        gammacon_squared = gammacon[index]^2 # Surface excess squared [Å^2]
+        pGM += 4Avog * ion[1]*1000 * gammacon_squared*1e-20 / (beta*h^2) # Gibbs-Marangoni pressure [m4.mol.kg/m5.mol.s2] -> [Pa], Eq.5
+    end
 
-    return (gammacon_q, tension, pGM);
+    return (conc_matrix, gammacon_q, tension, pGM)
 end;
 time_range = range(0.01, boundary, 100001);
 pGM_constants = (STconst, Avog, ionS, beta, h);
 pGM_param = (pGM_constants, ion_tot, n_salts);
-sol2 = pGM!(time_range, sol, pGM_param);
+sol2 = pGM!(time_range, sol, pGM_param, print_flag);
 # @btime sol2 = pGM!(time_range, sol, pGM_param)
